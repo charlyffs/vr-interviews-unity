d3d2ee59252be2e948583aaf6d54fb5e32b0187d
Carlos M <carlosedm@proton.me>
2023-04-12 16:48:43 -0700

    Add audio resources

--- Assets/Scripts/InterviewController.cs
+++ Assets/Scripts/InterviewController.cs
@@ -11,7 +11,6 @@ public class InterviewController : MonoBehaviour
     private Animator animator;
     public TextMeshProUGUI textMesh;
 
-    // Start is called before the first frame update
     void Start()
     {
         dummy = GameObject.FindGameObjectWithTag("Player");
